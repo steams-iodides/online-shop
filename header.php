f5f2215adb78ead69656065ce7079e62e10c8338
Natalia Iv <153074445+steams-iodides@users.noreply.github.com>
2023-12-06 16:59:04 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Shop</title>
+    <!-- Include CSS and JavaScript files -->
+</head>
+<body>
+    <header>
+        <!-- Navigation bar, user login status, etc. -->
+    </header>
