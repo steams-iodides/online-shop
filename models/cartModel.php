c85adaf8377de714bcb6e0b27ff265d85e03dc86
Natalia Iv <153074445+steams-iodides@users.noreply.github.com>
2023-12-06 17:02:08 +0700

    Create cartModel.php

---
+++ models/cartModel.php
@@ -0,0 +1,7 @@
+<?php
+    function getCartItems() {
+        // Retrieve cart items from the database or session
+    }
+
+    // Other cart-related functions (add to cart, remove from cart, etc.)
+?>
