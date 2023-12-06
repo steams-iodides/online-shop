cace5fde4088ee40087eea7c4703e9e67bfcf228
Natalia Iv <153074445+steams-iodides@users.noreply.github.com>
2023-12-06 17:00:52 +0700

    Create productModel.php

---
+++ models/productModel.php
@@ -0,0 +1,9 @@
+<?php
+    function getFeaturedProducts() {
+        // Retrieve featured products from the database
+    }
+
+    function getProductDetails($productId) {
+        // Retrieve product details from the database based on $productId
+    }
+?>
